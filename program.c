b1a1f33ded5442072078f78f888533f0664f7010
eduardorules <jesaldivia2@gmail.com>
2019-10-15 12:26:46 -0300

    patron 3

--- program.c
+++ program.c
@@ -32,26 +32,46 @@ void access_pattern1( char *data, int length )
 	for (int i = 0; i < length; i++) {
 	a = data[i];
 	}
-
+	// patron lineal, desde el fin a inicio pasa por todos dandole valores
+	// luego de inicio a fin lee los valores de todos
 	//TODO: Implementar
 }
 
 void access_pattern2( char *data, int length )
 {
 	int a;
-	for(int i = 0 ; i<1000 ; i++){
+	for(int i = 0 ; i<5000 ; i++){
 		a = lrand48()%length;
 		if(i%2==0){
 			data[a] = i; 
 		}
 		else{
-			data[a-1] = data[a];
+			if(a==0){
+				data[a+1] = data[a];	
+			}
+			else{
+				data[a-1] = data[a];
+			}
 		}
 	}
+	// patron aleatorio, 5000 veces entra a una posocion aleatoria
+	// si es par la iteracion escribe
+	// si es impar lee el valor del elegido y lo escribe en el antecesor.
 	// TODO: Implementar
 }
 
-void access_pattern3( char *cdata, int length )
+void access_pattern3( char *data, int length )
 {
+	for(int i = 0 ; i<100 ; i++){
+		int a = lrand48()%length;
+		for(int j = 0 ; j <20 ; j++){
+			int b = lrand48()%length;
+			if (a!=b){
+				data[a] = data[b]; 
+			}
+		}
+	}
+	// a diferencia de los otros dos patrones aca hay mucha mas repeticion en la frecuencia de uso de las paginas,
+	// en el patron 1 se usa una pagina y no se vuelve a usar, aca se repite muchas veces seguidas el uso de una pagina.
 	// TODO: Implementar
 }
